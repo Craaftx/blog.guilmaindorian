6a99547e5fd6f7a95dc2306e5559256082dd4bba
Craaftx <guilmain.dorian@gmail.com>
2020-02-09 11:35:23 +0100

    :sparkles: New Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      Created with{' '}
+      <span role="img" aria-label="Purple Heart">
+        💜
+      </span>{' '}
+      by Guilmain Dorian
+      <br />
+      <small>
+        <b>Creativity is contagious. Pass it on.</b>
+      </small>
+    </footer>
+  )
+}
+
+export default Footer
