e05fc4d4fa4bfe15f79c739d674cbfe4554f90be
Craaftx <guilmain.dorian@gmail.com>
2020-02-20 09:01:36 +0100

    :sparkles: Add navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,19 @@ import Helmet from 'react-helmet'
 
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Moon from './Moon'
 import './scss/main.scss'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <>
     <Helmet title="Home | Gatsby + WordPress" />
     <Navbar />
-    <main className="site-content">{children}</main>
-    <Footer />
-  </div>
+    <div className="site-wrapper">
+      <Moon starsAmout={100} />
+      <main className="site-content">{children}</main>
+      <Footer />
+    </div>
+  </>
 )
 
 export default TemplateWrapper
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,120 @@
-import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React, { useState } from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import PlanetBlue from '../planets/PlanetBlue'
+import PlanetAsteroid from '../planets/PlanetAsteroid'
+import PlanetRed from '../planets/PlanetRed'
+import PlanetPurple from '../planets/PlanetPurple'
 
-const Navbar = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
-          edges {
-            node {
-              title
-              slug
+const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allWordpressPage(sort: { fields: wordpress_id }, limit: 5) {
+            edges {
+              node {
+                title
+                slug
+              }
             }
           }
         }
-      }
-    `}
-    render={data => (
-      <nav className="navigation">
-        <div className="navigation-illustration">
-          <div className="moon"></div>
-          <div className="moon-full"></div>
-          {[...Array(200)].map(() => (
-            <span class="star"></span>
-          ))}
-        </div>
-        <div className="navigation-content">
-          <Link to="/" className="navigation-brand">
-            <p>Guilmain Dorian</p>
-            <span>Developer &amp; CSS Wizard</span>
-          </Link>
-          <div className="navigation-items">
-            <ul>
-              <li>
-                <Link href="#about">Home</Link>
-              </li>
-              <li>
-                <Link href="#projects">Categories</Link>
-              </li>
-            </ul>
+      `}
+      render={data => (
+        <nav className="navigation">
+          <div
+            class="navigation__mobile-burger"
+            onClick={e => {
+              setMenuIsOpen(menuIsOpen ? false : true)
+            }}
+          ></div>
+          <div
+            className={`navigation-content ${
+              menuIsOpen
+                ? 'navigation-content--opened'
+                : 'navigation-content--closed'
+            }`}
+          >
+            <div
+              class="navigation-content__mobile-background"
+              onClick={e => {
+                setMenuIsOpen(menuIsOpen ? false : true)
+              }}
+            ></div>
+            <div className="navigation-brand">
+              <AniLink
+                cover
+                direction="top"
+                bg="#42406f"
+                to="/"
+                className="navigation-item"
+              >
+                <div className="navigation-item__short">
+                  <div className="ring"></div>
+                </div>
+                <div className="navigation-item__link">
+                  <div>Dorian G</div>
+                  <small>CSS Blog ?</small>
+                </div>
+              </AniLink>
+            </div>
+            <div className="navigation-items">
+              <AniLink
+                cover
+                direction="top"
+                bg="#42406f"
+                to="/"
+                className="navigation-item"
+              >
+                <div className="navigation-item__planet">
+                  <PlanetBlue size="60px" />
+                </div>
+                <span className="navigation-item__link">Tutoriels</span>
+              </AniLink>
+              <AniLink
+                cover
+                direction="top"
+                bg="#42406f"
+                to="/"
+                className="navigation-item"
+              >
+                <div className="navigation-item__planet">
+                  <PlanetPurple size="60px" />
+                </div>
+                <span className="navigation-item__link">Séries</span>
+              </AniLink>
+              <AniLink
+                cover
+                direction="top"
+                bg="#42406f"
+                to="/"
+                className="navigation-item"
+              >
+                <div className="navigation-item__planet">
+                  <PlanetAsteroid size="60px" />
+                </div>
+                <span className="navigation-item__link">Ressources</span>
+              </AniLink>
+              <AniLink
+                cover
+                direction="top"
+                bg="#42406f"
+                to="/"
+                className="navigation-item"
+              >
+                <div className="navigation-item__planet">
+                  <PlanetRed size="60px" />
+                </div>
+                <span className="navigation-item__link">Expériences</span>
+              </AniLink>
+            </div>
           </div>
-        </div>
-      </nav>
-    )}
-  />
-)
+        </nav>
+      )}
+    />
+  )
+}
 
 export default Navbar
