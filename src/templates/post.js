521cf3897c439e3bf83bbfa84015e3d19a51e9b8
Craaftx <guilmain.dorian@gmail.com>
2020-02-29 15:25:10 +0100

    :sparkles: Add share buttons

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,69 +2,109 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  RedditShareButton,
+  RedditIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from 'react-share'
+import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
 import Layout from '../components/Layout'
 import PlanetDisplayer from '../components/PlanetDisplayer'
+import useSiteMetadata from '../utils/useSiteMetadata'
 
 export const BlogPostTemplate = ({
   content,
   categories,
+  slug,
   tags,
   title,
   date,
   author,
 }) => {
+  const { siteURL } = useSiteMetadata()
+  let disqusConfig = {
+    url: `${siteURL}/${slug}`,
+    identifier: slug,
+    title: title,
+  }
   return (
     <>
-    <PlanetDisplayer category={categories[0].slug} isOnSinglePage={true}/>
-    <article className="post type-post">
-      <div className="post-inner thin">
-        <div className="entry-content">
-          <header className="entry-header">
-            <h1 
-              dangerouslySetInnerHTML={{ __html: title }}
-              ></h1>
-            <div className="header-meta-wrapper">
-              <span>Posté le {date} par </span>{' '}
-              <Link to={`/author/${author.slug}`}>{author.name}</Link>{' '}
-              <span>dans</span>{' '}
-              {categories.map(category => (
-                <Link
-                  to={`/categories/${category.slug}/`}
-                  key={`${category.slug}cat`}
-                >
-                  {category.name}
-                </Link>
-              ))}
-            </div>
-          </header>
-        </div>
-        <div
-          className="entry-content"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-        <div className="entry-content">
-          <div className="post-meta-wrapper">
-            {tags && tags.length ? (
-              <div className="post-meta-tags">
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={`${tag.slug}tag`}>
-                      <Link to={`/tags/${tag.slug}/`}>{tag.name}</Link>
-                    </li>
-                  ))}
-                </ul>
+      <PlanetDisplayer category={categories[0].slug} isOnSinglePage={true} />
+      <article className="post type-post">
+        <div className="post-inner thin">
+          <div className="entry-content">
+            <header className="entry-header">
+              <h1 dangerouslySetInnerHTML={{ __html: title }}></h1>
+              <div className="header-meta-wrapper">
+                <span>Posté le {date} par </span>{' '}
+                <Link to={`/author/${author.slug}`}>{author.name}</Link>{' '}
+                <span>dans</span>{' '}
+                {categories.map(category => (
+                  <Link
+                    to={`/categories/${category.slug}/`}
+                    key={`${category.slug}cat`}
+                  >
+                    {category.name}
+                  </Link>
+                ))}
               </div>
-            ) : null}
-            <div className="post-meta-author">
-              <small>Auteur</small>
-              <img src={author.avatar_urls.wordpress_96} />
-              <Link to={`/author/${author.slug}`}>{author.name}</Link>
-              <p>{author.description}</p>
+            </header>
+          </div>
+          <div
+            className="entry-content"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+          <div className="entry-content">
+            <div className="post-meta-wrapper">
+              {tags && tags.length ? (
+                <div className="post-meta-tags">
+                  <ul className="taglist">
+                    {tags.map(tag => (
+                      <li key={`${tag.slug}tag`}>
+                        <Link to={`/tags/${tag.slug}/`}>{tag.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+              <div className="post-meta-author">
+                <small>Auteur</small>
+                <img src={author.avatar_urls.wordpress_96} />
+                <Link to={`/author/${author.slug}`}>{author.name}</Link>
+                <p>{author.description}</p>
+              </div>
+              <div className="post-meta-share">
+                <FacebookShareButton url={`${siteURL}/${slug}`}>
+                  <FacebookIcon size={32} round={true} />
+                </FacebookShareButton>
+                <TwitterShareButton url={`${siteURL}/${slug}`}>
+                  <TwitterIcon size={32} round={true} />
+                </TwitterShareButton>
+                <LinkedinShareButton url={`${siteURL}/${slug}`}>
+                  <LinkedinIcon size={32} round={true} />
+                </LinkedinShareButton>
+                <RedditShareButton url={`${siteURL}/${slug}`}>
+                  <RedditIcon size={32} round={true} />
+                </RedditShareButton>
+                <EmailShareButton url={`${siteURL}/${slug}`}>
+                  <EmailIcon size={32} round={true} />
+                </EmailShareButton>
+              </div>
+              <div className="post-meta-comments">
+                <CommentCount config={disqusConfig} placeholder={'...'} />
+                <Disqus config={disqusConfig} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </article>
+      </article>
     </>
   )
 }
@@ -82,6 +122,7 @@ const BlogPost = ({ data }) => {
       <BlogPostTemplate
         content={post.content}
         categories={post.categories}
+        slug={post.slug}
         tags={post.tags}
         title={post.title}
         date={post.date}
