d544b3849a675b885a4d6eee1e892eb08559c1bd
Craaftx <guilmain.dorian@gmail.com>
2020-02-29 15:24:05 +0100

    :sparkles: Extract AniLink in external component

---
+++ src/utils/AnimationLink.js
@@ -0,0 +1,21 @@
+import React, { Children } from 'react'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+const transitionColor = "#42406f";
+const transitionDirection = "top";
+
+const AnimationLink = ({ children, to, className }) => {
+  return (
+    <AniLink
+      cover
+      direction={transitionDirection}
+      bg={transitionColor}
+      to={to}
+      className={className}
+    >
+        {children}
+    </AniLink>
+  )
+}
+
+export default AnimationLink;
