73d5b2579a603aa506ea1238a963b8f3d9fd86c7
Guilmain Dorian <guilmain.dorian@gmail.com>
2020-09-14 09:55:08 +0200

    :bug: Fix node-gyp conflict with sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'Dorian G',
-		twitterHandle: '@Craaftx1',
-		author: '@Craaftx',
-    description: '',
-		siteURL: 'https://blog.guilmaindorian.com',
+    twitterHandle: '@Craaftx1',
+    author: '@Craaftx',
+    description: 'Blog technique où je parle de code, de CSS et de design.',
+    siteURL: 'https://blog.guilmaindorian.com',
   },
   plugins: [
     'gatsby-plugin-transition-link',
@@ -14,25 +14,25 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Open Sans', 'Lato']
-        }
-      }
+          families: ['Open Sans', 'Lato'],
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `guilmaindorian`
-      }
+        shortname: `guilmaindorian`,
+      },
     },
     {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'localhost/blog-headless',
+        baseUrl: 'blog-headless.guilmaindorian.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
-        protocol: 'http',
+        protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: false,
         auth: {},
@@ -44,7 +44,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     {
       // Removes unused css rules
-      resolve:'gatsby-plugin-purgecss',
+      resolve: 'gatsby-plugin-purgecss',
       options: {
         // Activates purging in gatsby develop
         develop: true,
