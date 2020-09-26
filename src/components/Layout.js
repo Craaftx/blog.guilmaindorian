a76599584031b9f4d393598965d8c0f53a3c4da5
Guilmain Dorian <guilmain.dorian@gmail.com>
2020-09-26 22:23:48 +0200

    :sparkles: Update default helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,33 +8,40 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import Moon from './Moon'
 import '../scss/main.scss'
+import useSiteMetadata from '../utils/useSiteMetadata'
 
-const TemplateWrapper = ({ children }) => (
-  <>
-    <Helmet title="Blog | Dorian G" />
-    <Navbar />
-    <div className="site-wrapper">
-      <Moon starsAmout={100} />
-      <main className="site-content">{children}</main>
-      <Footer />
-    </div>
-    <CookieConsent
-      disableStyles
-      buttonClasses="cookies-bar__button"
-      containerClasses="cookies-bar"
-      contentClasses="cookies-bar__content"
-      buttonText="Compris"
-      location="bottom"
-    >
-      <Cookie />
-      Hey ! Je dois te préciser que le blog utilise des cookies. En explorant le
-      site, tu acceptes leur utilisation selon les conditions de la
-      <AnimationLink to="/politique-de-confidentialite">
-        &nbsp; politique de confidentialité
-      </AnimationLink>
-      .
-    </CookieConsent>
-  </>
-)
+const TemplateWrapper = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Navbar />
+      <div className="site-wrapper">
+        <Moon starsAmout={100} />
+        <main className="site-content">{children}</main>
+        <Footer />
+      </div>
+      <CookieConsent
+        disableStyles
+        buttonClasses="cookies-bar__button"
+        containerClasses="cookies-bar"
+        contentClasses="cookies-bar__content"
+        buttonText="Compris"
+        location="bottom"
+      >
+        <Cookie />
+        Hey ! Je dois te préciser que le blog utilise des cookies. En explorant
+        le site, tu acceptes leur utilisation selon les conditions de la
+        <AnimationLink to="/politique-de-confidentialite">
+          &nbsp; politique de confidentialité
+        </AnimationLink>
+        .
+      </CookieConsent>
+    </>
+  )
+}
 
 export default TemplateWrapper
